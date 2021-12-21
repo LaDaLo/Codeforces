f54af682b4af63a529444d636c2f1ec8da0e094b
LaDaLo <lordorochiz@gmail.com>
2021-12-21 23:07:17 +0700

    round674

---
+++ round674/1426B.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define fast_io ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> arr(n);
+    
+}       
+ 
+int main()
+{
+    fast_io;
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
---
+++ round674/1426C.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define fast_io ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+
+void solve() {
+    
+}       
+ 
+int main()
+{
+    fast_io;
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
